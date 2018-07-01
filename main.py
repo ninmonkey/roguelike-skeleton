ddaf8b7061067c0dbe40ae9f2454d615e38805c4
Jake Bolton <ninmonkeys@gmail.com>
2018-07-01 10:12:23 -0500

    renders fully using tdl

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 PATH_APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 root_console = None
+isDone = False
+player_x, player_y = 1, 1
 
 
 def random_font():
@@ -20,7 +22,13 @@ def random_font():
 
 def init():
     global root_console
-    #
+    global player_x, player_y
+    
+    # entities
+    player_x = SCREEN_WIDTH // 2
+    player_y = SCREEN_HEIGHT // 2
+
+    # render
     path = os.path.join(PATH_APP_ROOT, 'fonts', 'arial10x10.png')
     tdl.set_font(path, greyscale=True, altLayout=True)
 
@@ -30,10 +38,49 @@ def init():
 
 def loop():
     global root_console
+    global player_x, player_y
 
-    while not tdl.event.is_window_closed():
-        root_console.draw_char(1, 1, '@', bg=None, fg=(255,255,255))
+    while not isDone and not tdl.event.is_window_closed():
+        root_console.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
         tdl.flush()
+
+        handle_input()
+
+
+# def handle_input_realtime():
+#
+#     keypress = False
+#     for event in tdl.event.get():
+#         if event.type == 'KEYDOWN':
+#             user_input = event
+#             keypress = True
+#     if not keypress:
+#         return
+
+
+def handle_input():
+    global isDone
+    global player_x, player_y
+
+    user_input = tdl.event.key_wait()
+
+    if user_input.key == 'ENTER' and user_input.alt:
+        tdl.set_fullscreen(not tdl.get_fullscreen())
+
+    elif user_input.key == 'ESCAPE':
+        isDone = True
+        return
+
+    if user_input.key == 'UP':
+        player_y -= 1
+    elif user_input.key == 'DOWN':
+        player_y += 1
+    elif user_input.key == 'LEFT':
+        player_x -= 1
+    elif user_input.key == 'RIGHT':
+        player_x += 1
+
+
 
 
 def main():
--- readme.md
+++ readme.md
@@ -1,17 +1,5 @@
-# Test libtcod packages
+# tdl / libtcod docs and references
 
-`libtcod-cffi`
-vs
-`tdl`
-
-# libtcod
-
-tdl docs:
-    https://python-tdl.readthedocs.io/en/latest/tdl.html
-    
-tcod docs:
-    http://roguecentral.org/doryen/data/libtcod/doc/1.5.1/index2.html?c=false&cpp=false&cs=false&py=true&lua=false
-    
 fonts:
     https://github.com/libtcod/python-tcod/tree/master/fonts/libtcod
     
@@ -20,3 +8,13 @@ colors:
     
 examples:
     https://github.com/libtcod/python-tcod/tree/master/examples
+
+tdl docs:
+    https://python-tdl.readthedocs.io/en/latest/tdl.html
+    
+tcod docs:
+    http://roguecentral.org/doryen/data/libtcod/doc/1.5.1/index2.html?c=false&cpp=false&cs=false&py=true&lua=false
+    
+key constants:
+    https://pythonhosted.org/tdl/tdl.event.KeyEvent-class.html#key
+    
