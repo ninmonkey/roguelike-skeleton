7e5d7c4c447957289d8622cd79edaadb30ba108c
Jake Bolton <ninmonkeys@gmail.com>
2018-07-01 10:25:00 -0500

    changed input to use dictionaries

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 PATH_APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 root_console = None
+con = None  # back buffer
 isDone = False
 player_x, player_y = 1, 1
 
 
-def random_font():
-    font_dir = os.path.join(PATH_APP_ROOT, 'fonts')
-    fonts = os.listdir(font_dir)
-    path = os.path.join(font_dir, random.choice(fonts))
-    libtcod.console_set_custom_font(path, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+# def random_font():
+#     font_dir = os.path.join(PATH_APP_ROOT, 'fonts')
+#     fonts = os.listdir(font_dir)
+#     path = os.path.join(font_dir, random.choice(fonts))
+#     libtcod.console_set_custom_font(path, libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
 
 def init():
-    global root_console
+    global root_console, con
     global player_x, player_y
-    
+
     # entities
     player_x = SCREEN_WIDTH // 2
     player_y = SCREEN_HEIGHT // 2
@@ -31,56 +32,70 @@ def init():
     # render
     path = os.path.join(PATH_APP_ROOT, 'fonts', 'arial10x10.png')
     tdl.set_font(path, greyscale=True, altLayout=True)
-
     root_console = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title='tcod demo', fullscreen=False)
+    con = tdl.Console(SCREEN_WIDTH, SCREEN_HEIGHT)
     tdl.setFPS(LIMIT_FPS)
 
 
 def loop():
-    global root_console
+    global root_console, con
     global player_x, player_y
+    global isDone
 
     while not isDone and not tdl.event.is_window_closed():
-        root_console.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
+        # draw
+        con.draw_char(player_x, player_y, '@', bg=None, fg=(255, 255, 255))
+        root_console.blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0)
         tdl.flush()
 
-        handle_input()
+        # clear buffer?
+        con.draw_char(player_x, player_y, ' ', bg=None)
+
+        # input
+        for event in tdl.event.get():
+            if event.type == 'KEYDOWN':
+                user_input = event
+                break
+        else:
+            user_input = None
+
+        if not user_input:
+            continue
+
+        action = handle_input(user_input)
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
+            isDone = True
+
+        if fullscreen:
+            tdl.set_fullscreen(not tdl.get_fullscreen())
 
 
-# def handle_input_realtime():
-#
-#     keypress = False
-#     for event in tdl.event.get():
-#         if event.type == 'KEYDOWN':
-#             user_input = event
-#             keypress = True
-#     if not keypress:
-#         return
-
-
-def handle_input():
-    global isDone
-    global player_x, player_y
-
-    user_input = tdl.event.key_wait()
+def handle_input(user_input):
+    # Movement keys
+    if user_input.key == 'UP':
+        return {'move': (0, -1)}
+    elif user_input.key == 'DOWN':
+        return {'move': (0, 1)}
+    elif user_input.key == 'LEFT':
+        return {'move': (-1, 0)}
+    elif user_input.key == 'RIGHT':
+        return {'move': (1, 0)}
 
     if user_input.key == 'ENTER' and user_input.alt:
-        tdl.set_fullscreen(not tdl.get_fullscreen())
-
+        return {'fullscreen': True}
     elif user_input.key == 'ESCAPE':
-        isDone = True
-        return
+        return {'exit': True}
 
-    if user_input.key == 'UP':
-        player_y -= 1
-    elif user_input.key == 'DOWN':
-        player_y += 1
-    elif user_input.key == 'LEFT':
-        player_x -= 1
-    elif user_input.key == 'RIGHT':
-        player_x += 1
-
-
+    return {}
 
 
 def main():
